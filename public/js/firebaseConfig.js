93697ae2964b1169540be4d5ead1a3372f41adc4
John Perry <johnadducul2001@gmail.com>
2023-11-17 04:15:39 +0800

    latest

---
+++ public/js/firebaseConfig.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCd3Xzw_AmhSbMcfbwHyMrKFR_GxK2OqrA",
+  authDomain: "phcp-smartlight.firebaseapp.com",
+  databaseURL: "https://phcp-smartlight-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "phcp-smartlight",
+  storageBucket: "phcp-smartlight.appspot.com",
+  messagingSenderId: "208487238165",
+  appId: "1:208487238165:web:0ea79bc854d0344c525c1c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  
